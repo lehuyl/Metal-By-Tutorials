3731e785a367fe8d659a74611883d4130a696be0
lehuyl <stevenle18@gmail.com>
2026-03-19 09:34:12 -0700

    Use indexed vertices

---
+++ chapter4/Vertex/Vertex/Quad.swift
@@ -0,0 +1,54 @@
+import MetalKit
+
+struct Vertex {
+    var x: Float
+    var y: Float
+    var z: Float
+}
+struct Quad {
+    var oldVertices: [Vertex] = [
+        Vertex(x: -1, y: 1, z: 0), // triangle 1
+        Vertex(x: 1, y: -1, z: 0),
+        Vertex(x: -1, y: -1, z: 0),
+        Vertex(x: -1, y: 1, z: 0), // triangle 2
+        Vertex(x: 1, y: 1, z: 0),
+        Vertex(x: 1, y: -1, z: 0)
+    ]
+    var vertices: [Vertex] = [
+        Vertex(x: -1, y: 1, z: 0),
+        Vertex(x: 1, y: 1, z: 0),
+        Vertex(x: -1, y: -1, z: 0),
+        Vertex(x: 1, y: -1, z: 0)
+    ]
+    
+    var indices: [UInt16] = [
+        0, 3, 2,
+        0, 1, 3
+    ]
+    let indexBuffer: MTLBuffer
+    let vertexBuffer: MTLBuffer
+    
+    init(device: MTLDevice, scale: Float = 1) {
+        vertices = vertices.map {
+            Vertex(x: $0.x * scale, y: $0.y * scale, z: $0.z * scale)
+        }
+        guard let vertexBuffer = device.makeBuffer(
+            bytes: vertices,
+            length: MemoryLayout<Vertex>.stride * vertices.count,
+            options: []) else {
+            fatalError("Unable to create quad vertex buffer")
+        }
+        
+        guard let indexBuffer: MTLBuffer = device.makeBuffer(
+            bytes: &indices,
+            length: MemoryLayout<UInt16>.stride * indices.count,
+            options: []) else {
+            fatalError("Unable to create quad index buffer")
+        }
+        
+        self.vertexBuffer = vertexBuffer
+        self.indexBuffer = indexBuffer
+    }
+}
+
+
